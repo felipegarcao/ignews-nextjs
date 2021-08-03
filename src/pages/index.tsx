8673a0ffba0ffb8f40b26623b8dad5d7148581ab
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-03 15:41:51 -0300

    Titulo Dinamico Por Pagina

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import styles from '../styles/home.module.scss'
+import styles from '../styles/home.module.scss';
+import Head from 'next/head';
+
+
 export default function Home() {
   return (
     <>
+    <Head>
+      <title>Incio | ig.news</title>
+    </Head>
     <h1 className={styles.title}>
       Hello World - IgNews
     </h1>
