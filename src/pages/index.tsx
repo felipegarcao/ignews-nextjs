6f36ec1d88b3c8a893d39ecd51a4922dd7076393
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-03 17:36:41 -0300

    Componente Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,25 @@
 import Head from 'next/head';
 
+import styles from './home.module.scss'
+
 
 export default function Home() {
   return (
     <>
     <Head>
-      <title>Incio | ig.news</title>
+      <title>Home | ig.news</title>
     </Head>
-    <h1>
-      Hello World - IgNews
-    </h1>
+    <main className={styles.contentContainer} >
+      <section className={styles.hero}>
+        <span>👏 Hey, Welcome</span>
+        <h1>News About <br />the <span>React</span> World</h1>
+        <p>
+          Get access to all the Publications <br />
+          <span>For $9.90 month</span>
+        </p>
+      </section>
+      <img src="/images/avatar.svg" alt="Girl Coding" />
+    </main>
     </>
   )
 }
