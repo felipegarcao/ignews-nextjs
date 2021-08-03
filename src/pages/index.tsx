3b7f7bb9ba412f9276f46c8a722be0498ab3bd65
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-03 15:18:08 -0300

    INSTALANDO SASs no Projeto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,10 @@
+import styles from '../styles/home.module.scss'
 export default function Home() {
   return (
-    <h1>Hello World - IgNews</h1>
+    <>
+    <h1 className={styles.title}>
+      Hello World - IgNews
+    </h1>
+    </>
   )
 }
