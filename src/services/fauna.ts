e38b967f50f5bd0325562456ae373396aadd93f0
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-05 17:15:44 -0300

    Salvando o Email do Usuario no FAUNADB

--- src/services/fauna.ts
+++ src/services/fauna.ts
@@ -1,6 +1,6 @@
 import { Client } from 'faunadb'
 
 
-export const fauan = new Client({
-  secret: process.env.FAUNADB_KEY
+export const fauna = new Client({
+  secret: process.env.FAUNADB_KEY_SECRET
 })
