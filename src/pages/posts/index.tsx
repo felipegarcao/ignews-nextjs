f65da1eb9dc63576ec55d7c0812e9baf23669343
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-08-10 16:42:38 -0300

    Consumindo a Api do Prismic, Depois de Ter Configurando ele no Site de Maneira Simples

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,5 +1,10 @@
 import Head from 'next/head'
 import styles from './styles.module.scss';
+import Prismic from '@prismicio/client'
+
+import { getPrismicClient } from '../../services/prismic';
+
+import { GetStaticProps } from 'next';
 
 export default function Posts(){
   return (
@@ -30,4 +35,23 @@ export default function Posts(){
       </main>
     </>
   );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient()
+
+  const response = await prismic.query([
+    Prismic.predicates.at('document.type', 'publication')
+  ],{
+    fetch: ['publication.title', 'publication.content'],
+    pageSize: 100,
+  })
+
+  console.log(JSON.stringify(response, null, 2));
+
+  return {
+    props: {
+
+    }
+  }
 }
